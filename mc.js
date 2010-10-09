40453cf9c301ecc5304bf33f17290f9813125785
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-10-08 22:19:29 -0700

    started memblox namespace for restructuring game mechanics

--- mc.js
+++ mc.js
@@ -1,8 +1,165 @@
 // this is the main javascript file.
+(function(window, document, undefined) {
+  if(!window.console || !console.log){
+    console.log = alert;
+  }
+  /* randomInt
+   *  @param low : the low number in the return set
+   *  @param high : the high number in the return set
+   */
+  function randomInt(low, high){
+    return Math.floor(Math.random() * (high - (low - 1))) + low;
+  }
+  // EventTarget class acts as base for custom events
+  function EventTarget(){
+    this._listeners = {};
+  };
+  EventTarget.prototype = {
+    constructor: EventTarget,
+    addListener: function(type, listener){
+      if (typeof this._listeners[type] == "undefined"){
+        this._listeners[type] = [];
+      }
+      this._listeners[type].push(listener);
+    },
+    fire: function(event){
+      if(typeof event == "string"){
+        event = {type: event};
+      }
+      if(!event.target){
+        event.target = this;
+      }
+      if(!event.type){
+        throw new Error("Event object missing 'type' property.");
+      }
+      if(this._listeners[event.type] instanceof Array){
+        var listeners = this._listeners[event.type];
+        for(var i = 0, lis; lis = listeners[i++];){
+          lis.call(this, event);
+        }
+      }
+    },
+    removeListener: function(type, listener){
+      if(this._listeners[type] instanceof Array){
+        var listeners = this._listeners[type];
+        for (var i = 0, lis; lis = listeners[i++];){
+          if (lis === listener){
+            listeners.splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+  };
+  // block object inherits from EventTarget class
+  function block(){
+    EventTarget.call(this);
+    var active = false;
+    var control = $("<div class='blox'/>");
+    this.control = control;
+    this.control.click = this.flip;
+    this.top = control.offset().top;
+    this.left = control.offset().left;
+    return this;
+  }
+  block.prototype = new EventTarget();
+  block.prototype.constructor = block;
+  block.prototype.flip = function(){
+    this.fire('flip');
+  };
+  block.prototype.move = function(){
+    this.fire('move');
+  };
+  block.prototype.fall = function(){
+    this.fire('fall');
+  };
+  block.prototype.activate = function(){
+    this.fire('activate');
+  }
+  block.prototype.deactivate = function(){
+    this.fire('deactivate');
+  }
+  function flipBlock(ev){
+    //animate the flipping of the block
+    console.log("flipping block");
+    //check for any other flipped blocks and check for match
+    console.log("checking for match");
+    //animate the flip back
+    console.log("flipping back");
+  }
+  /**
+   * moveBlock
+   *  @param x : -1 == left, 1 == right, 0 == nochange;
+   *  @param y : -1 == up, 1 == down, 0 == nochange;
+   */
+  function moveBlock(x, y){
+    var x, y, nx, ny;
 
-(function() {
-var tetris = {
-	soundEnabled: true,
+    // check that space in new location is empty
+    console.log("checking destination");
+    // move if you can
+    console.log("moving or not");
+  }
+  function clearBlock(ev){
+
+  }
+  // end block object definition
+  // board object inherits from EventTarget class
+  function board(){
+    EventTarget.call(this);
+  }
+  function control(control, size, position){
+    function F(control, size, position){this.size = size, this.position = position, this.control = control};
+    F.prototype.read = function(){return control.innerText;}
+    F.prototype.write = function(msg, clear){control.innerText = (clear == false)? msg : control.innerText + '\n' + msg;}
+    var newcontrol = new F(control, size, position);
+    return newcontrol;
+  }
+  var memblox = {
+    options : {
+      soundEnabled: true,
+      bufferMin: 3,
+      bufferMax: 10,
+      pSize:40,
+      boardHeight: 0,
+      boardWidth: 0,
+      numberOfMatches: 16,
+    },
+    io : {
+      canvas : {},
+      boardDisplay: {},
+      scoreDisplay: {},
+      levelDisplay: {}
+    },
+    actions: {
+      flipBlock : function(block){/**/return;},
+      moveBlock : function(block, direction){/**/return;},
+    },
+    events: {
+      blockCleared : function(block){/**/return;},
+      blockMoved : function(block, direction){/**/return;},
+      blockFell : function(block, spaces){/**/return;},
+      blockFlipped : function(block){/**/return;},
+      levelCleared : function(level){/**/return;}
+    },
+    environment: {
+      board : [],
+      player : "",
+      score : 0,
+      level : 0,
+      flipped : {
+        count : 0,
+        blocks : []
+      }
+    },
+    assets: {
+      images : [],
+      sounds : []
+    }
+  }
+  // tetris definition below
+  var tetris = {
+    soundEnabled: true,
     streamCursor: 0,
     deck: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
     streamSize: 200,
@@ -178,7 +335,7 @@ var tetris = {
       return Math.floor((Math.random() * (tetris.numberOfMatches))+1);
     },
     createSquare:function(x,y,type) {
-	
+
       var el = document.createElement('div');
       el.className = 'square unflip panel moving type'+type;
       el.setAttribute("data-value", this.stream[this.streamCursor]);
@@ -686,7 +843,8 @@ var tetris = {
 	},
 };
 tetris.init();
-})();
+window.memblox = memblox;
+})(window, window.document);
 
 if (!Array.prototype.eachdo) {
   Array.prototype.eachdo = function(fn) {
