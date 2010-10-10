c575afcf159bb03f6637f78e708dc0a4ac706f69
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2010-10-09 22:44:18 -0700

    added match test

--- memblox2.js
+++ memblox2.js
@@ -1,95 +1,4 @@
-var speed = 4;
-var BLOCK_HEIGHT = 40;
-var BLOCK_WIDTH  = 40;
-var GAME_LEFT_BOUNDARY = 0;
-var GAME_RIGHT_BOUNDARY = GAME_LEFT_BOUNDARY + (BLOCK_WIDTH*8);
-var GAME_UPPER_BOUNDARY = 0;
-var GAME_LOWER_BOUNDARY = GAME_UPPER_BOUNDARY + (BLOCK_HEIGHT*12);
-
-// These are the delays in secs. between block movements for each level
-// index 0 is the delay for level 1, etc.
-//
-var FALL_DELAY = new Array(0.5, 0.45, 0.4, 0.35, 0.25, 0.2, 0.15, 0.1, 0.05); 
-
-var FAST_FALL_DELAY = 0.05;
-function randomInt(low, high){
-  return Math.floor(Math.random() * (high - (low - 1))) + low;
-}
-Sprite.extend('Block',{	url: '/images/sprites/mario/' + randomInt(1, 8) + '.png',
-        width: BLOCK_HEIGHT,
-	height: BLOCK_WIDTH,
-	hitRect: new Rect(1, 1, BLOCK_HEIGHT, BLOCK_WIDTH),
-	collisions: true,
-	state: 'falling',
-	bumpedSide: false,
-        setup: function(clock){
-          this.setImage("/images/sprites/mario/" + randomInt(1, 8) + ".png");
-        },
-	logic: function(clock) {
-		this.bumpedSide = false;
-		this.prevX = this.x;
-		this.prevY = this.y;
-		this[ this.state ](clock);
-	}
-});
-Block.add({
-	falling: function(clock) {
-		// now move the sprite, horizontal only first
-		//
-                var hit;
-                var dir;
-                if (Effect.Game.isKeyDown("right")) {
-                  hit = this.move( this.width, 0 );
-                  dir = -1;
-                }else if(Effect.Game.isKeyDown("left")){
-                  hit = this.move( -this.width, 0 );
-                  dir = 1;
-                }
- // and detenct collision
-		if (this.didCollideX(hit)) {
-		    	this.bumpedSide = true;
-			// if we hit something solid, stop our horiz movement
-			this.x = this.x + (this.width * dir) ;
-		}
-		this.xd = 0;
-		
-		// now move the sprite vertically
-		//
-                var hit = this.move( 0, 1 * speed );
-                if (this.didCollideY(hit)) {
-                        // if we hit something solid, stop our vert movement
-                        this.y--;                
-                        var splane = Effect.Port.getPlane("Blocks");
-                        if(this.y >= 0){
-                          splane.createSprite("Block",{url: '/images/sprites/mario/' + randomInt(1, 8) + '.png',x: 160, y:-40});
-                        }else{
-                          alert("GAME OVER SUCKA!");
-                        }
-                        this.state = 'stuck';
-                }
-	}
-});
-Block.add({
-	didCollideX: function(hit) {
-		return (hit && (hit.target.solid ||
-		       (hit.target.type == 'Block' && hit.target.state == 'stuck'))) ||
-		       this.x < GAME_LEFT_BOUNDARY ||
-		       this.x > GAME_RIGHT_BOUNDARY - this.width;
-	},
-	
-	didCollideY: function(hit) {
-		return (hit && (hit.target.solid ||
-		       (hit.target.type == 'Block' && hit.target.state == 'stuck'))) ||
-		       this.y > GAME_LOWER_BOUNDARY - this.height;
-	}
-});
-
-Block.add({
-	stuck: function(clock) {
-		this.xd = this.yd = 0;
-	}
-});
-(function(window, document, undefined) {
+var memblox = (function(window, document, undefined) {
   if(!window.console || !console.log){
     console = {};
     console.log = alert;
@@ -189,8 +98,104 @@ Block.add({
       sounds : []
     }
   }
-  window.memblox = memblox;
+  return memblox;
 })(window, window.document)
+
+var speed = 4;
+var BLOCK_HEIGHT = 40;
+var BLOCK_WIDTH  = 40;
+var GAME_LEFT_BOUNDARY = 0;
+var GAME_RIGHT_BOUNDARY = GAME_LEFT_BOUNDARY + (BLOCK_WIDTH*8);
+var GAME_UPPER_BOUNDARY = 0;
+var GAME_LOWER_BOUNDARY = GAME_UPPER_BOUNDARY + (BLOCK_HEIGHT*12);
+
+// These are the delays in secs. between block movements for each level
+// index 0 is the delay for level 1, etc.
+//
+var FALL_DELAY = new Array(0.5, 0.45, 0.4, 0.35, 0.25, 0.2, 0.15, 0.1, 0.05); 
+
+var FAST_FALL_DELAY = 0.05;
+function randomInt(low, high){
+  return Math.floor(Math.random() * (high - (low - 1))) + low;
+}
+Sprite.extend('Block',{	url: '/images/sprites/' + memblox.environment.themes[memblox.environment.theme] + '/' + randomInt(1, 8) + '.png',
+        width: BLOCK_HEIGHT,
+	height: BLOCK_WIDTH,
+	hitRect: new Rect(1, 1, BLOCK_HEIGHT, BLOCK_WIDTH),
+	collisions: true,
+	state: 'falling',
+	bumpedSide: false,
+        setup: function(clock){
+          this.matchNumber = randomInt(1, 8);
+          this.setImage("/images/sprites/" + memblox.environment.themes[memblox.environment.theme] + "/" + this.matchNumber + ".png");
+        },
+	logic: function(clock) {
+		this.bumpedSide = false;
+		this.prevX = this.x;
+		this.prevY = this.y;
+		this[ this.state ](clock);
+	}
+});
+Block.add({
+	falling: function(clock) {
+		// now move the sprite, horizontal only first
+		//
+                var hit;
+                var dir;
+                if (Effect.Game.isKeyDown("right")) {
+                  hit = this.move( this.width, 0 );
+                  dir = -1;
+                }else if(Effect.Game.isKeyDown("left")){
+                  hit = this.move( -this.width, 0 );
+                  dir = 1;
+                }
+ // and detenct collision
+		if (this.didCollideX(hit)) {
+		    	this.bumpedSide = true;
+			// if we hit something solid, stop our horiz movement
+			this.x = this.x + (this.width * dir) ;
+		}
+		this.xd = 0;
+		
+		// now move the sprite vertically
+		//
+                var hit = this.move( 0, 1 * speed );
+                if (this.didCollideY(hit)) {
+                        // if we hit something solid, stop our vert movement
+                        this.y = this.y - (1 * speed);                
+                        var splane = Effect.Port.getPlane("Blocks");
+                        if(this.y >= 0){
+                          splane.createSprite("Block",{url: '/images/sprites/mario/' + randomInt(1, 8) + '.png',x: 160, y:-40});
+                        }else{
+                          alert("GAME OVER SUCKA!");
+                        }
+                        this.state = 'stuck';
+                }
+	}
+});
+Block.add({
+  
+})
+Block.add({
+	didCollideX: function(hit) {
+		return (hit && (hit.target.solid ||
+		       (hit.target.type == 'Block' && hit.target.state == 'stuck'))) ||
+		       this.x < GAME_LEFT_BOUNDARY ||
+		       this.x > GAME_RIGHT_BOUNDARY - this.width;
+	},
+	
+	didCollideY: function(hit) {
+		return (hit && (hit.target.solid ||
+		       (hit.target.type == 'Block' && hit.target.state == 'stuck'))) ||
+		       this.y > GAME_LOWER_BOUNDARY - this.height;
+	}
+});
+
+Block.add({
+	stuck: function(clock) {
+		this.xd = this.yd = 0;
+	}
+});
 
 Effect.Game.addEventListener( 'onLoadGame',function(){
   memblox.init();
@@ -207,7 +212,21 @@ Effect.Game.addEventListener( 'onLoadGame',function(){
   Effect.Port.attach( splane );
   Effect.Port.addEventListener( 'onMouseDown', function(pt, buttonIdx){
     // there needs to be useful
-    alert( "You clicked " + pt.x + " by " + pt.y );
+    var splane = Effect.Port.getPlane("Blocks");
+    var sprite = splane.lookupSpriteFromGlobal(pt);
+    if(sprite){
+      if(memblox.environment.flipped.blocks.length == 1){
+        if (sprite.matchNumber == memblox.environment.flipped.blocks[0].matchNumber && sprite !== memblox.environment.flipped.blocks[0]){
+          console.log(sprite.id);
+          // add drop state physics
+        }
+        memblox.environment.flipped.blocks = [];
+      }else{
+         memblox.environment.flipped.blocks.push(sprite);
+      }
+      alert(sprite.matchNumber);
+    }
+    //alert( "You clicked " + pt.x + " by " + pt.y );
   });
   Effect.Game.loadLevel( 'Default', function(){
   splane = Effect.Port.getPlane("Blocks");
